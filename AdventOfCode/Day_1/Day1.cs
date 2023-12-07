769315f7e31cd4c1dad38ff92b0ce8e1235ed3e4
LavenderPixl <annajacobsen1999@hotmail.com>
2023-12-07 14:34:01 +0100

    Add project files.

---
+++ AdventOfCode/Day_1/Day1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        string[] input = File.ReadAllLines("../../../Day_1/Day1Input.txt");
+        //string[] input = { "a23aee2", "sad76lk1ælæ3" };
+        string collection;
+        int result;
+
+
+        public Day1()
+        {
+            foreach (var line in input)
+            {
+                List<char> chars = new List<char>();
+
+                //Converts the (string) line into a Char Array.
+                char[] charArray = line.ToCharArray();
+                foreach (var item in charArray)
+                {
+                    //Checks if each Char in the Array is a number, and then Adds them to a char list.
+                    if (Char.IsNumber(item))
+                    {
+                        chars.Add(item);
+                    }
+                }
+
+                collection += chars[0].ToString() + "" + chars[chars.Count() - 1].ToString() + ",";
+            }
+            string[] resArr = collection.Split(',');
+            foreach (var item in resArr)
+            {
+                Int32.TryParse(item, out int val);
+                result += val;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1 day1 = new Day1();
+        }
+    }
+}
